f7725019cab75e46caf8261d309769fb84b259b3
buivanthien <57850451+buivanthien1@users.noreply.github.com>
2021-03-16 18:12:54 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -4,14 +4,14 @@ del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
-net user administrator Tuanx04 /add >nul
+net user administrator BVTVN-ThienBui /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo VPS đã đc tạo! Kết nối VPS của bạn bằng Remote Desktop (RDP).
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo Dịch vụ NGROK không chạy
 echo User: Administrator
-echo Pass: Tuanx04
+echo Pass: BVTVN-ThienBui
 
 
 
